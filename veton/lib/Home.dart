457f534dc09d260ea378d9c4c2ba063a7d20b6cf
saadkhawar <saadkhawar289@gmail.com>
2022-08-27 01:37:54 +0500

    search done

--- veton/lib/Home.dart
+++ veton/lib/Home.dart
@@ -240,6 +240,7 @@ from: e.from
     List<LocalStorageCalls> callsData = [];
     var box = Boxes.getTransactions();
     callsData = box.values.toList();
+
     return callsData.toList();
   }
 
@@ -373,14 +374,52 @@ from: e.from
 
   void searchLead(String query) {
     final suggestion = searchLeadList.where((lead) {
-      final leadtile = lead.name.toUpperCase();
-      final input = searchController.text.toUpperCase();
+      final leadtile = lead.name.toLowerCase();
+      final input = searchController.text.toLowerCase();
       return leadtile.contains(input);
     }).toList();
     setState(() {
       searchLeadList = suggestion;
     });
   }
+
+  ///............................................
+  ///................
+
+  void searchBook(String query) {
+    final leads = searchLeadList.where((lead) {
+      final titleLower = lead.name.toLowerCase();
+      final authorLower = lead.number.toLowerCase();
+      final searchLower = query.toLowerCase();
+
+      return titleLower.contains(searchLower) ||
+          authorLower.contains(searchLower);
+    }).toList();
+
+    setState(() {
+      this.query = query;
+     this. leadList = leads;
+    });
+  }
+
+  Widget buildSearch() => SearchWidget(
+    text: query,
+    hintText: 'Name or Number ',
+    onChanged: searchBook,
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   @override
   void dispose() {
@@ -543,40 +582,7 @@ from: e.from
                                         const SizedBox(
                                           height: 15,
                                         ),
-                                        // Container(
-                                        //
-                                        //     width: MediaQuery.of(context)
-                                        //             .size
-                                        //             .width *
-                                        //         0.70,
-                                        //     decoration: BoxDecoration(
-                                        //         color: Colors.grey[300],
-                                        //         borderRadius:
-                                        //             BorderRadius.circular(
-                                        //                 30)),
-                                        //     child: Padding(
-                                        //       padding: const EdgeInsets.only(
-                                        //           left: 18.0),
-                                        //       child: TextField(
-                                        //         controller: searchController,
-                                        //         decoration: InputDecoration(
-                                        //           prefixIcon: const Icon(
-                                        //               Icons.search),
-                                        //           suffixIcon: GestureDetector(
-                                        //               onTap: () {
-                                        //                 setState(() {
-                                        //                   searchLeadList = leadList;
-                                        //                   searchController
-                                        //                       .clear();
-                                        //                 });
-                                        //               },
-                                        //               child: const Icon(
-                                        //                   Icons.close)),
-                                        //           border: InputBorder.none,
-                                        //         ),
-                                        //         onChanged: searchLead,
-                                        //       ),
-                                        //     )),
+                                        buildSearch(),
                                         const SizedBox(
                                           height: 20,
                                         ),
@@ -772,4 +778,66 @@ class LeadModel {
 class Boxes {
   static Box<LocalStorageCalls> getTransactions() =>
       Hive.box<LocalStorageCalls>('CallsHistory');
+}
+
+
+class SearchWidget extends StatefulWidget {
+  final String text;
+  final ValueChanged<String> onChanged;
+  final String hintText;
+
+  const SearchWidget({
+    Key? key,
+    required this.text,
+    required this.onChanged,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  _SearchWidgetState createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final styleActive = TextStyle(color: Colors.black);
+    final styleHint = TextStyle(color: Colors.black54);
+    final style = widget.text.isEmpty ? styleHint : styleActive;
+
+    return Container(
+      height: 42,
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        border: Border.all(color: Colors.black26),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          icon: Icon(Icons.search, color: style.color),
+          suffixIcon: widget.text.isNotEmpty
+              ? GestureDetector(
+            child: Icon(Icons.close, color: style.color),
+            onTap: () {
+              controller.clear();
+
+              widget.onChanged('');
+              FocusScope.of(context).requestFocus(FocusNode());
+
+            },
+          )
+              : null,
+          hintText: widget.hintText,
+          hintStyle: style,
+          border: InputBorder.none,
+        ),
+        style: style,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
 }
